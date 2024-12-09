18689bb5e8bfaf7c9280e1bb511b15ad07dd7d45
t.melko <timamelko25@gmail.com>
2024-12-10 02:08:14 +0300

    added main

---
+++ 4 lab/main.py
@@ -0,0 +1,82 @@
+from itertools import combinations
+import networkx as nx
+import matplotlib.pyplot as plt
+
+class Graph:
+    def __init__(self, filename):
+        self.adj_matrix = self.read_matrix(filename)
+        self.V = len(self.adj_matrix)
+
+    def read_matrix(self, file_path):
+        with open(file_path, 'r') as f:
+            adj_matrix = [list(map(int, line.strip().split())) for line in f]
+        return adj_matrix
+
+    def is_vertex_cover(self, vertex_set):
+        edges = [(i, j) for i in range(self.V) for j in range(i + 1, self.V) if self.adj_matrix[i][j] == 1]
+        for u, v in edges:
+            if u not in vertex_set and v not in vertex_set:
+                return False
+        return True
+
+    def find_min_vertex_cover(self):
+        vertices = range(self.V)
+        for size in range(1, self.V + 1):
+            for subset in combinations(vertices, size):
+                if self.is_vertex_cover(subset):
+                    return subset
+        return None
+
+    def is_independent_set(self, vertex_set):
+        for i in vertex_set:
+            for j in vertex_set:
+                if i != j and self.adj_matrix[i][j] == 1:
+                    return False
+        return True
+
+    def find_max_independent_set(self):
+        global X, max_size
+        X = []
+        max_size = 0
+        stack = []
+        stack.append(([], list(range(self.V))))
+
+        while stack:
+            S, T = stack.pop()
+            if len(S) > max_size:
+                X = S[:]
+                max_size = len(S)
+            for v in T:
+                if all(self.adj_matrix[v][u] == 0 for u in S):
+                    new_T = [x for x in T if x != v]
+                    stack.append((S + [v], new_T))
+        return X
+
+    def print_results(self):
+        min_vertex_cover = self.find_min_vertex_cover()
+        print("Наименьшее вершинное покрытие:", min_vertex_cover)
+        max_independent_set = self.find_max_independent_set()
+        if self.is_independent_set(max_independent_set):
+            print("Наибольшее независимое множество вершин:", max_independent_set)
+        else:
+            print("Полученное множество не является независимым")
+
+def visualize_graph(adj_matrix):
+    G = nx.Graph()
+    for i in range(len(adj_matrix)):
+        for j in range(i, len(adj_matrix)):
+            if adj_matrix[i][j] == 1:
+                G.add_edge(i, j)
+    plt.figure(figsize=(8, 6))
+    pos = nx.spring_layout(G)
+    nx.draw(
+        G, pos, with_labels=True, node_color="lightblue", node_size=500, font_size=10, font_weight="bold"
+    )
+    plt.title("Graph Visualization")
+    plt.show()
+
+if __name__ == "__main__":
+    filename = "graph1.txt"
+    graph = Graph(filename)
+    graph.print_results()
+    visualize_graph(graph.adj_matrix)
